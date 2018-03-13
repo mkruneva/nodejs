8abeb356ac6071bfcbb0d3612b5233760c863484
mkruneva <marta.kruneva@gmail.com>
2018-03-13 14:06:47 +0000

    ulp-jasmine-node

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
 const JasmineConsoleReporter = require('jasmine-console-reporter');
+const jasmineNode = require('gulp-jasmine-node');
 
 const specReporter = new JasmineConsoleReporter({
     colors: 1, // (0|false)|(1|true)|2 
@@ -11,9 +12,15 @@ const specReporter = new JasmineConsoleReporter({
 });
 
 
-gulp.task('test', () =>
-    gulp.src('test/main.spec.js')
-    .pipe(jasmine({
-        reporter: specReporter
-    }))
-);
+// gulp.task('test', () =>
+//     gulp.src('test/main.spec.js')
+//     .pipe(jasmine({
+//         reporter: specReporter
+//     }))
+// );
+
+gulp.task('test', function() {
+    return gulp.src(['test/*spec.js']).pipe(jasmineNode({
+        timeout: 10000
+    }));
+});

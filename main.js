6c28efa03386380ebdcc30df2684215328682e5e
mkruneva <marta.kruneva@gmail.com>
2018-03-14 13:51:25 +0000

    refactor

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 'use strict';
+(function() {
+    const colors = require('colors');
+    const fs = require('fs');
 
-exports.changeColors = {
-    nodeArg: function(argArray) {
-        // return process.argv[2]
-        return argArray[2]
-    },
-    arrayColors: function() {
-        const colors = require('colors');
-        return [colors.red, colors.green, colors.blue, colors.yellow, colors.magenta]
-    },
-    readFile: function() {
-        const fs = require('fs');
-        return fs.readFile(nodeArg, "utf8", function(err, data) {
-            if (err) throw err;
-            let str = data.toString();
-            let result = str.split(" ");
-            result.forEach((word, i) => {
-                process.stdout.write(arrayColors[i % arrayColors.length](word + ' '));
+    const arrayColors = [
+        colors.red,
+        colors.green,
+        colors.blue,
+        colors.yellow,
+        colors.magenta
+    ];
+
+    exports.changeColors = {
+        filename: function(argArray) {
+            return argArray[2]
+        },
+        readfile: function(fileName, cb) { // give a file name reads the file and returns an array of all words
+            return [];
+        },
+        paintwords: function(colors, arrayOfWords, write) { // give and array of words and colorisation functions it colorises the words and prints them on the screen
+            arrayOfWords.forEach((word, i) => {
+                write(colors[i % colors.length](word + ' '));
             });
-        });
+        },
+        main: function() {
+            const fileName = this.filename(process.argv);
+            console.log(fileName);
+            // this.readfile(fileName, function(err, words) {
+            //     paintwords(arrayColors, words, process.stdout.write);
+            // });
+        },
+        // readFile: function() {
+
+        //     return fs.readFile(filename, "utf8", function(err, data) {
+        //         if (err) throw err;
+        //         let str = data.toString();
+        //         let result = str.split(" ");
+        // result.forEach((word, i) => {
+        //     process.stdout.write(arrayColors[i % arrayColors.length](word + ' '));
+        // });
+        //     });
+        // }
     }
-}
+
+    exports.changeColors.main();
+})();
--- test/main.spec.js
+++ test/main.spec.js
@@ -2,19 +2,55 @@
 
 const main = require('../main');
 
-// console.log('changeColors.add js is', main.changeColors.add);
-
-describe("nodeArg function", function() {
-    let nodeArgumentFun;
+describe("node Arguments file name function", function() {
+    let filenameFunction;
     let mockArray;
 
     it("should return 3-rd argument of a given array", () => {
-        nodeArgumentFun = main.changeColors.nodeArg;
+        filenameFunction = main.changeColors.filename;
         mockArray = [1, 2, 3];
 
-        expect(nodeArgumentFun(mockArray)).toBe(3);
+        expect(filenameFunction(mockArray)).toBe(3);
+    });
+    it("should return undefined if array has less than 3 elements", () => {
+        filenameFunction = main.changeColors.filename;
+        mockArray = [1, 2];
+
+        expect(filenameFunction(mockArray)).toBeUndefined();
     });
 });
+
+describe("main function", function() {
+    let mainFunction;
+
+    it("should be defined", function() {
+        mainFunction = main.changeColors.main;
+
+        expect(mainFunction).toBeDefined();
+    })
+});
+// describe("paint words function", function() {
+//     let mockColors;
+//     let mockWords;
+//     let mockWrite;
+
+//     it("should print the words from the array and on the screen", () => {
+//         // spyOn(rest, 'init');
+//         // post = new Post(rest);
+//         // expect(rest.init).toHaveBeenCalled();
+
+//         mockColors = [];
+//         mockWords = ['one', 'two', 'three'];
+//         mockWrite = function(word) {};
+//         let writeFunction = process.stdout.write();
+
+//         main.changeColors.paintwords([], [], mockWrite);
+
+//         expect(writeFunction).toHaveBeenCalled();
+
+//     });
+// });
+
 
 // main.js should be the second node argument
 // nodeArg shoudl be the 3rd node arg 
